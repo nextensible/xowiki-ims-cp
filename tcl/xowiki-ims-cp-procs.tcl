d39aa88fa763dd315f52d02a9c8e8c45cf765eef
Michael Aram <mischa@s15289513.onlinehome-server.info>
2009-07-02 09:05:52 +0200

    Improving import and export
    Still buggy and away from mature, but its possible to import a ZIP file into the wiki
    and export it again afterwards.

--- tcl/xowiki-ims-cp-procs.tcl
+++ tcl/xowiki-ims-cp-procs.tcl
@@ -21,15 +21,59 @@ namespace eval ::xowiki::ims::cp {
         -table_name "xowiki_ims_cp" \
         -superclass ::xowiki::Package
 
-    # Todo Dont do a subclass - do a Service Contarct for import
+
+    Package ad_instproc export_wiki_as_cp {} {} {
+        # Get all Pages
+        set pageids [list]
+        set sql [::xowiki::Page instance_select_query -with_subtypes 1 -folder_id [my set folder_id] -with_children true]
+        db_foreach retrieve_instances $sql {
+             # not the folder object 
+             if {$name eq "::[my set folder_id]"} {
+                 continue
+             }
+             lappend pageids $item_id
+        }
+
+        set files [list]
+
+        foreach item_id $pageids {
+            set cri [::xo::db::CrClass get_instance_from_db -item_id $item_id]
+            lappend cr_items $cri
+            #ds_comment "MISCHA2 [$cri serialize] [$cri info methods]"
+            #    ns_log notice "file detected  [acs_root_dir]/content-repository-content-files[$cri set text]"
+
+            if {[file isfile "[acs_root_dir]/content-repository-content-files[$cri set text]"]} {
+                set f [::xoutil::File create [my autoname exportfile] -location "[acs_root_dir]/content-repository-content-files[$cri set text]"]
+                $f set name [$cri set name]
+                lappend files $f
+            } else {
+                ns_log notice "others detected"
+                set f [::xoutil::TempFile create [my autoname exportfile]]
+                $f set content [$cri set text]
+                $f set name [$cri set name]
+                $f save
+                lappend files $f
+            }
+        }
+
+
+        ContentPackage create cp -location "[acs_root_dir]/packages/ims-cp[ns_tmpnam]"
+        cp add_files $files
+        set pif [cp pack]
+        $pif deliver
+    }
+
+
+    #TODO  Dont make a subclass - make a Service Contract for import
     Class ContentPackage -superclass ::ims::cp::ContentPackage
 
-    ContentPackage instproc initialize {} {
-        Package initialize
-        my set xo_pkg_obj $package_id
-        #ds_comment "CP: [[self] serialize]"
-        #ds_comment "P: [$package_id serialize]"
-    }
+  #  # This is not beautiful
+  #  ContentPackage instproc initpkg {} {
+  #      Package initialize
+  #      my set xo_pkg_obj $package_id
+  #      #ds_comment "CP: [[self] serialize]"
+  #      #ds_comment "P: [$package_id serialize]"
+  #  }
 
     # DEPRECATED - We now mount into the current instance
     ContentPackage instproc mount_to_site_node {} {
@@ -46,140 +90,94 @@ namespace eval ::xowiki::ims::cp {
 
     # todo move this elsewhere
     ContentPackage instproc empty_target_wiki {} {
-        db_foreach instance_select \
-            [::xowiki::Page instance_select_query \
-                 -folder_id [[my set xo_pkg_obj] set folder_id] \
-                 -with_children true
-            ] {
-                    [my set xo_pkg_obj] delete -name $name
+        set sql [::xowiki::Page instance_select_query -with_subtypes 1 -folder_id [[my set xo_pkg_obj] set folder_id] -with_children true]
+        db_foreach retrieve_instances $sql {
+            # preserve the folder object - we get errors if not
+             if {$name eq "::[[my set xo_pkg_obj] set folder_id]"} {
+                 continue
+             }
+          permission::require_write_permission -object_id $item_id
+          ::[my set xo_pkg_obj] delete -item_id $item_id -name $name
+        }
+    }
+
+    # By default, we import only those items, that are mentioned in the Manifest, which 
+    # is correct. Using the switch, we import everything that was in the zippackage.
+    ContentPackage instproc import_to_wiki_instance { {-include_dead_files false} } {
+        my instvar xo_pkg_obj
+        # TODO dirty
+        if {$include_dead_files eq false} {
+            ns_log notice "LIVE oNLY"
+            foreach imsfile [my get_unique_file_objects] {
+                #set cp [$imsfile find_content_package]
+                set realfile [$imsfile set fileobj]
+                $realfile set package_id [$xo_pkg_obj set id]
+                $realfile set folder_id  [$xo_pkg_obj set folder_id]
+                $realfile mixin add ::xowiki::ims::cp::File
+                $realfile to_xowiki
             }
-    }
-
-    ContentPackage instproc import_to_wiki_instance {} {
-        foreach r [[my set manifest]::resources children] {
-            $r mixin add Resource
-            $r import_to_xowiki
+        } else {
+            ns_log notice "DEAD TOO"
+            foreach realfile [[self]::files children] {
+                my log "FILE $realfile ready to import"
+                $realfile set package_id [$xo_pkg_obj set id]
+                $realfile set folder_id  [$xo_pkg_obj set folder_id]
+                $realfile mixin add ::xowiki::ims::cp::File
+                $realfile to_xowiki
+            }
         }
     }
 
-
-    #::ims::cp::PackageInterchangeFile instmixin add ::xowiki::ims::cp::ContentPackage
-
-#    Object importer
+#    # We extend the XoWiki Objects with the ability to
+#    # "write" them as temp_files to disk. Maybe this can be done 
+#    # more beautiful
 #
-#    importer proc import {} {
+#    # this is deprecated
+#    Class Page
+#    ::xowiki::Page instmixin add ::xowiki::ims::cp::Page
 #
-#
-#        ::xowiki::PlainPage create test \
-#            -set text {asd} \
-#            -set title {sdasd} \
-#            -name "NAME" \
-#            -parent_id $folder_id \
-#            -package_id $package_id
-#
-#        test save_new
-#
-#
-#
+#    Page instproc get_cp_filename {} {
+#        set filename [my name]
+#        #TODO consider MIME and remove language ?
+#        regsub -all : $filename _ filename
+#        return $filename.html
 #    }
 #
-    ################################
-    #                              #
-    #  ::xowiki::ims::cp::Package  #
-    #                              #
-    ################################
-
-#    Class Package
-#
-#
-#    
-#
-#    Package instproc as_ims_cp { } {
-#        my set filename [my instance_name].zip
-#
-#        set sql [::xowiki::Page instance_select_query -folder_id [my folder_id] -with_subtypes true]
-#
-#        set cr_item_ids [db_list get_all_pages $sql]
-#
-#        my create_from_cr_item_ids -cr_item_ids $cr_item_ids
-#
-#        my pack_to_pif
-#
+#    Page instproc write_to_file { {-path "/tmp"} } {
+#        set fn "$path/[my get_cp_filename]"
+#        set fid [open $fn w]
+#        puts $fid [my text]
+#        puts $fid [my serialize]
+#        close $fid
+#        return $fn
 #    }
-#   ::xowiki::Package instmixin add ::ims::cp::Package
-#   ::xowiki::Package instmixin add ::xowiki::ims::cp::Package
 
-
- #   Package instproc as_ims_cp {-url} {
- #       ::xowiki::Package initialize -url $url
-
- #       my set filename [$package_id instance_name].zip
-
- #       set sql [::xowiki::Page instance_select_query -folder_id [$package_id folder_id] -with_subtypes true]
-
- #       set cr_item_ids [db_list get_all_pages $sql]
-
- #       my create_from_cr_item_ids -cr_item_ids $cr_item_ids
-
- #       my pack_to_pif
-
- #   }
-
- #   ::xowiki::ims::cp::Package instmixin add ::ims::cp::Package
-
-    # We extend the XoWiki Objects with the ability to
-    # "write" them as temp_files to disk. Maybe this can be done 
-    # more beautiful
-
-    Class Page
-    ::xowiki::Page instmixin add ::xowiki::ims::cp::Page
-
-    Page instproc get_cp_filename {} {
-        set filename [my name]
-        #TODO consider MIME and remove language ?
-        regsub -all : $filename _ filename
-        return $filename.html
-    }
-
-    Page instproc write_to_file { {-path "/tmp"} } {
-        set fn "$path/[my get_cp_filename]"
-        set fid [open $fn w]
-        puts $fid [my text]
-        puts $fid [my serialize]
-        close $fid
-        return $fn
-    }
-
-
-
-
-
-
-    #
-    # MARKER - FIX HERE
-    #
-
-    Class Resource
-
-    # TODO Do we need dependencies here??
-    Resource instproc import_to_xowiki {} {
-        foreach imsfile [[self]::files children] {
-            set cp [$imsfile find_content_package]
-            ds_comment "CP IS: $cp"
-            set xo_pkg_obj ::[$cp set xo_pkg_obj]
-            ds_comment "XOCPOBJ IS: $xo_pkg_obj"
-            ds_comment "XOPKGOBJ: [$xo_pkg_obj serialize]"
-
-
-            ds_comment "FILE TAG [$imsfile serialize]"
-            set realfile [$imsfile set fileobj]
-            $realfile set package_id [$xo_pkg_obj set id]
-            $realfile set folder_id  [$xo_pkg_obj set folder_id]
-            $realfile mixin add ::xowiki::ims::cp::File
-            ds_comment "REAL [$realfile serialize]"
-            $realfile to_xowiki
-        }
-    }
+#    Class Resource
+#
+#    # TODO Do we need dependencies here??
+#    Resource instproc import_to_xowiki {} {
+#        ds_comment "RESSOURCE: [my serialize]"
+#        foreach imsfile [[self]::files children] {
+#
+#            #we check, whether we already created a file with this url
+#
+#
+#            set cp [$imsfile find_content_package]
+#            #ds_comment "CP IS: $cp"
+#            set xo_pkg_obj ::[$cp set xo_pkg_obj]
+#            #ds_comment "XOCPOBJ IS: $xo_pkg_obj"
+#            #ds_comment "XOPKGOBJ: [$xo_pkg_obj serialize]"
+#
+#
+#            #ds_comment "FILE TAG [$imsfile serialize]"
+#            set realfile [$imsfile set fileobj]
+#            $realfile set package_id [$xo_pkg_obj set id]
+#            $realfile set folder_id  [$xo_pkg_obj set folder_id]
+#            $realfile mixin add ::xowiki::ims::cp::File
+#            #ds_comment "REAL [$realfile serialize]"
+#            $realfile to_xowiki
+#        }
+#    }
 
 
 
@@ -210,6 +208,7 @@ namespace eval ::xowiki::ims::cp {
     }
 
     File instproc to_xowiki {} {
+        #ds_comment "TO WIKI: [my serialize]"
         #FIXME
 
         set package_id [my set package_id]
@@ -218,39 +217,36 @@ namespace eval ::xowiki::ims::cp {
         #set folder_id [[::ims::cp::controller::pkg set xo_pkg_obj] folder_id]
             switch -- [my mime_type] {
                 "text/html" {
-                    ds_comment "HTML: [my serialize]"
-                    set o [::xowiki::Page new \
+                    #ds_comment "HTML: [my serialize]"
+                    set o [::xowiki::Page new -destroy_on_cleanup \
                         -set text [list [my set content] [my set mime_type]] \
                         -set title [my set name] \
                         -name [my set name] \
                         -parent_id $folder_id \
                         -package_id $package_id]
-                $o save_new
+                    $o save_new
                 }
                 "text/plain" {
-                    ds_comment "PLAIN: [my serialize]"
-                    set o ::xowiki::PlainPage new \
+                    #ds_comment "PLAIN: [my serialize]"
+                    set o [::xowiki::PlainPage new -destroy_on_cleanup \
                         -set text [list [my set content] [my set mime_type]] \
                         -set title [my set name] \
                         -name [my set name] \
                         -parent_id $folder_id \
                         -package_id $package_id]
-                $o save_new
+                    $o save_new
                 }
                 default {
-                    ds_comment "FILE: [my serialize]"
-                   set o [ ::xowiki::File new \
-                        -set text [my set content] \
-                        -set title [my set set name] \
+                   #ds_comment "FILE: [my serialize]"
+                   set f [::xowiki::File new -destroy_on_cleanup \
+                        -title [my set name] \
                         -name [my set name] \
                         -parent_id $folder_id \
                         -package_id $package_id]
+                   $f set import_file [my set location]
+                   $f save_new
                 }
-                $o save_new
             }
-
     }
-
-
 }
 
